a3c98fd1f255069cb5ad439376c2ec8ac4d4ad02
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2025-07-09 22:13:36 +0600

    Added Pagination First and count

--- application/rest-app/rest_app/controller/raw_db_api_controller.py
+++ application/rest-app/rest_app/controller/raw_db_api_controller.py
@@ -3,6 +3,7 @@ from mweb import Controller
 from mweb_crud import mweb_paginate_endpoint, mweb_upload_endpoint, mweb_endpoint
 from rest_app.dto.person_dto import PersonCreateDTO, PersonUploadDTO, PersonDetailsDTO
 from rest_app.model.author import Author
+from mweb_orm import and_
 
 controller_url = "/api/v1/raw-db"
 raw_db_api_controller = Controller(
@@ -43,7 +44,26 @@ async def read_all():
     result = await Author.query.read_all()
     print(result)
     result2 = await Author.select(Author.name).read_all()
-    return "Saved all"
+    single = await Author.query.where(
+        and_(Author.id == 10)
+    ).first()
+    print(single)
+
+    count = await Author.query.count()
+    print(count)
+
+    conditional_count = await Author.query.where(Author.id > 500).count()
+    print(conditional_count)
+
+    pagination = await Author.query.paginate()
+    print(f"Item Count: {len(pagination.items)}")
+    print(pagination)
+
+    pagination = await Author.query.paginate(item_per_page=-1)
+    print(f"Item Count: {len(pagination.items)}")
+    print(pagination)
+
+    return "Read all"
 
 
 @raw_db_api_controller.route("/details/<int:model_id>", methods=['GET'])
@@ -68,5 +88,3 @@ async def delete(model_id: int):
 @mweb_upload_endpoint(request_obj=PersonUploadDTO, mweb_message_response=True)
 async def upload():
     pass
-
-
